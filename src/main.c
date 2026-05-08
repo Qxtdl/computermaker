d0494403db5e5c164eaf96659d639abf92904de2
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-05-08 14:12:46 -0300

    not really fix, just moved chat_cleanup call from function main into function destroy

--- src/main.c
+++ src/main.c
@@ -28,6 +28,7 @@ void init(void) {
 }
 
 void destroy(void) {
+    chat_cleanup();
     exit(0);
 }
 
@@ -47,5 +48,4 @@ int main() {
     window_create("comm", 800, 600, init, destroy, tick, render);
     chat_add_message("comm", APP_RELEASE_STRING);
     window_loop();
-    chat_cleanup();
 }
