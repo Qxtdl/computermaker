c66b4ac63af25cfd42809c3ca537bc353ab9dfa2
yu34567890 <133307919+yu34567890@users.noreply.github.com>
2026-05-10 14:40:24 +0300

    update xor and xnor

--- src/world/block/xnor.c
+++ src/world/block/xnor.c
@@ -2,19 +2,14 @@
 
 void _xnor_tick(block_t *block) {
     if (block->gate.num_inputs == 0) {
-        block->gate.new_state = STATE_ON;
+        block->gate.new_state = STATE_OFF;
         return;
     }
-    int count = 0;
+
+    int state = 0;
     for (int i = 0; i < block->gate.inputs_size; i++) {
         if (!block->gate.inputs[i].gate) continue;
-        if (block->gate.inputs[i].gate->state == STATE_ON) {
-            count++;
-        }
+        state ^= block->gate.inputs[i].gate->state;
     }
-
-    if (count % 2)
-        block->gate.new_state = STATE_OFF;
-    else
-        block->gate.new_state = STATE_ON;
+    block->gate.new_state = !state;
 }
