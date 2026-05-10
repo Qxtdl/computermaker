1f5fcdf74347940f9d5284f191afd338c152f187
yu34567890 <133307919+yu34567890@users.noreply.github.com>
2026-05-10 21:14:05 +0300

    Change XNOR block state from OFF to ON

--- src/world/block/xnor.c
+++ src/world/block/xnor.c
@@ -2,7 +2,7 @@
 
 void _xnor_tick(block_t *block) {
     if (block->gate.num_inputs == 0) {
-        block->gate.new_state = STATE_OFF;
+        block->gate.new_state = STATE_ON;
         return;
     }
 
